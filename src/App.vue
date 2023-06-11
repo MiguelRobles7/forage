58e948f64d10c2ead4c13bbd47539df77c0cb95f
Miguel Robles <donnaldmiguelrobles7@gmail.com>
2023-06-12 01:56:29 +0800

    feat: add app base

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// Imports here
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  </main>
+</template>
