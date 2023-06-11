39f8476f14de7be7993883a20325851c85f766ee
Miguel Robles <donnaldmiguelrobles7@gmail.com>
2023-06-12 02:33:19 +0800

    feat: add multi-page setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
 // Imports here
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header></header>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <br />
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
 
   <main>
     <h1 className="text-3xl font-bold underline">Hello world!</h1>
   </main>
+  <RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>This is a home page</h1>
+  </main>
+</template>
