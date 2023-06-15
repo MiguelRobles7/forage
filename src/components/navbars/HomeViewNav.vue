5b2bce3ebe96cc5d1632497fc40f052b4fb4c824
Johndayll Arizala <johndayll_arizala@dlsu.edu.ph>
2023-06-15 20:36:20 +0800

    feat add font in navbars

--- src/components/navbars/HomeViewNav.vue
+++ src/components/navbars/HomeViewNav.vue
@@ -1,29 +1,29 @@
 <template>
     <nav class="fixed top-0 bg-transparent text-white w-screen px-14 py-7">
         <div class="flex items-center">
-            <div class="text-3xl font-bold">
+            <div class="font-Rowdies text-3xl font-bold">
                 Forage
             </div>
             <div class="flex flex-grow justify-end">
-                <a href="" class="mr-5 text-xl font-semibold underline underline-offset-2">
+                <a href="" class="mr-5 text-xl font-bold font-SpaceGrotesk underline decoration-1 underline-offset-2">
                     Home
                 </a>
-                <a href="" class="mr-5 text-xl font-semibold">
+                <a href="" class="mr-5 text-xl font-bold font-SpaceGrotesk">
                     Highlights
                 </a>
-                <a href="" class="mr-5 text-xl font-semibold">
+                <a href="" class="mr-5 text-xl font-bold font-SpaceGrotesk">
                     Top Reviews 
                 </a>
-                <a href="" class="mr-5 text-xl font-semibold">
+                <a href="" class="mr-5 text-xl font-bold font-SpaceGrotesk">
                    Hot 
                 </a>
-                <a href="" class="mr-5 text-xl font-semibold">
+                <a href="" class="mr-5 text-xl font-bold font-SpaceGrotesk">
                    Near You 
                 </a>
-                <button class="mr-5 text-lg bg-gradient-to-r from-[#5DDB8F] to-[#35C56F] rounded-s rounded-e px-5 font-semibold border-solid border-black border-[1px]">
+                <button class="mr-5 text-lg bg-gradient-to-r from-[#5DDB8F] to-[#35C56F] rounded-s rounded-e px-5 font-bold font-SpaceGrotesk border-solid border-black border-[1px]">
                     Sign Up
                 </button>
-                <button class="text-lg bg-gradient-to-r from-[#5DDB8F] to-[#35C56F] rounded-s rounded-e px-5 font-semibold border-solid border-black border-[1px]">
+                <button class="bg-gradient-to-r from-[#5DDB8F] to-[#35C56F] rounded-s rounded-e px-5 font-bold font-SpaceGrotesk border-solid border-black border-[1px] text-lg">
                     Log In
                 </button>
             </div>
