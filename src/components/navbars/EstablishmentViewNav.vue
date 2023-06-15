c601ea835be895c68658d772d8cdcb165e12e822
Johndayll Arizala <johndayll_arizala@dlsu.edu.ph>
2023-06-15 16:34:36 +0800

    feat add navbars

---
+++ src/components/navbars/EstablishmentViewNav.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="fixed top-0 bg-black text-white w-screen px-14 py-7">
+        <div class="flex items-center">
+            <div class="text-3xl font-bold">
+                Forage
+            </div>
+            <div class="flex flex-grow justify-end">
+                <!--No Form Yet-->
+                <img src="../../assets/search.png" class="relative top-[7px] left-7 w-[17px] h-[17px]">
+                <input type="text" class="mr-5 rounded-md text-black pl-10 w-[300px] placeholder:text-black" placeholder="Search...">
+                <a href="" class="mr-5 text-xl font-semibold underline underline-offset-2">
+                    Home
+                </a>
+                <button class="mr-5 text-lg bg-gradient-to-r from-[#5DDB8F] to-[#35C56F] rounded-s rounded-e px-5 font-semibold border-solid border-black border-[1px]">
+                    Sign Up
+                </button>
+                <button class="text-lg bg-gradient-to-r from-[#5DDB8F] to-[#35C56F] rounded-s rounded-e px-5 font-semibold border-solid border-black border-[1px]">
+                    Log In
+                </button>
+            </div>
+        </div>
+    </nav>
+</template>
---
+++ src/components/navbars/HomeViewNav.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="fixed top-0 bg-transparent text-white w-screen px-14 py-7">
+        <div class="flex items-center">
+            <div class="text-3xl font-bold">
+                Forage
+            </div>
+            <div class="flex flex-grow justify-end">
+                <a href="" class="mr-5 text-xl font-semibold underline underline-offset-2">
+                    Home
+                </a>
+                <a href="" class="mr-5 text-xl font-semibold">
+                    Highlights
+                </a>
+                <a href="" class="mr-5 text-xl font-semibold">
+                    Top Reviews 
+                </a>
+                <a href="" class="mr-5 text-xl font-semibold">
+                   Hot 
+                </a>
+                <a href="" class="mr-5 text-xl font-semibold">
+                   Near You 
+                </a>
+                <button class="mr-5 text-lg bg-gradient-to-r from-[#5DDB8F] to-[#35C56F] rounded-s rounded-e px-5 font-semibold border-solid border-black border-[1px]">
+                    Sign Up
+                </button>
+                <button class="text-lg bg-gradient-to-r from-[#5DDB8F] to-[#35C56F] rounded-s rounded-e px-5 font-semibold border-solid border-black border-[1px]">
+                    Log In
+                </button>
+            </div>
+        </div>
+    </nav>
+</template>
