0580f5bdaa626b0a9113a064f9ade658dd3f0983
Johndayll Arizala <johndayll_arizala@dlsu.edu.ph>
2023-06-20 05:42:41 +0800

    feat: add computed properties for establishmentview

--- src/views/EstablishmentView.vue
+++ src/views/EstablishmentView.vue
@@ -9,25 +9,40 @@ export default {
       reviewCount: 421, rating: 4.8}
       ],
       reviews: [
-        {user_id: 1, restaurant_id:1, rating: 5, upvotes: 30, review: "Is good, is chill"}
+        {user_id: 1, restaurant_id:1, rating: 5, upvotes: 30, review: "Is good, is chill"},
+        {user_id: 2, restaurant_id:2, rating: 5, upvotes: 30, review: "Is good, is chill"}
       ],
       review_images: [
-
+        {user_id: 1, restaurant_id:1, rating: 5, upvotes: 30, review: "Is good, is chill"},
+        {user_id: 2, restaurant_id:2, rating: 5, upvotes: 30, review: "Is good, is chill"}
       ],
       users: [
         {user_id: 1, name: "Jadnel", password: "ako nalang kasi", image: "", location: "puso nya", description: "Sya parin bro", }
       ],
       menu: [
-        {restaurant_id: 1, item_name: "Original Latte", price: 139.00, image: ""}
+        {restaurant_id: 1, item_name: "Original Latte", price: 139.00, image: ""},
+        {restaurant_id: 1, item_name: "Caramel Latte", price: 149.00, image: ""},
+        {restaurant_id: 2, item_name: "Vanilla Latte", price: 149.00, image: ""}
       ]
     }
   },
 
   mounted() {
     console.log("miss na kita, legit")
+  },
 
-    if(this.id == 1) {
-      console.log("joke lang")
+  computed: {
+    restaurant() {
+      return this.restaurants.filter((restaurant) => {
+        return restaurant.restaurant_id === Number(this.id)})[0]
+    },
+
+    menu_items() { 
+      return this.menu.filter((row) => {return row.restaurant_id === Number(this.id)})
+    },
+
+    current_reviews() {
+      return this.reviews.filter((review) => {return review.restaurant_id === Number(this.id)})
     }
   }
 }
@@ -37,5 +52,8 @@ export default {
   <div>
     <h1>Establishment</h1>
     <div>The id is {{ id }}</div>
+    <div>The restaurant is {{ restaurant.name }}</div>
+    <div>The menu is {{ menu_items }}</div>
+    <div>The reviews are {{ current_reviews }}</div>
   </div>
 </template>
