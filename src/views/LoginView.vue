075ec283d7eca7ddf800378d8f70671c5ca6322a
Rafa <58285574+JsRphlMrtnz@users.noreply.github.com>
2023-06-19 20:42:01 +0800

    Add LoginView base

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,6 +2,30 @@
 
 <template>
   <main>
-    <h1>Login</h1>
+    <div class="backdrop">
+      <div class="grid h-screen place-items-center">
+        <div class="rounded-xl bg-gradient-to-b from-[#F4F2EB] to-[#B8B6B2] min-w-[75vw] min-h-[75vh]">
+          <div class="grid grid-flow-col">
+            <div>Insert bg Here</div>
+            <div class="mt-10 ml-20">
+              <span class="font-TTInterphases font-bold text-4xl"><span class="text-[#2A7E58]">Login</span> your account.</span>
+              <div>
+                <form action=""></form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+</style>
