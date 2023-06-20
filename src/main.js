fd6c3941a69888f4254af49163f05cda171a79c5
Miguel Robles <donnaldmiguelrobles7@gmail.com>
2023-06-20 13:00:40 +0800

    refactor: convert navbar styling from tailwind to bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/style.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as bootstrap from 'bootstrap'
 
 const app = createApp(App)
 
