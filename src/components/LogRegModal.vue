c70ba465aa04f2d80448725e29c49a3cff110708
Miguel Robles <donnaldmiguelrobles7@gmail.com>
2023-06-22 00:01:05 +0800

    fix; refactor: fix curve; convert log modal styling

--- src/components/LogRegModal.vue
+++ src/components/LogRegModal.vue
@@ -1,38 +1,36 @@
 <script>
 export default {
-    props: {
-        type: String,
-        typeText: String
-    },
-    data() {
-        return {
-        }
-    }
+  props: {
+    type: String,
+    typeText: String
+  },
+  data() {
+    return {}
+  }
 }
 </script>
 
 <template>
   <main>
     <div class="backdrop">
-      <div class="grid h-screen place-items-center">
-        <div class="rounded-xl bg-gradient-to-b from-[#F4F2EB] to-[#B8B6B2] min-w-[75vw] min-h-[75vh]">
-          <div class="grid grid-flow-col">
-            <div class="bg">
-                <div class="flex min-h-[75vh] items-center ml-14">
-                    <div class="flex flex-col">
-                        <span class="font-TTInterphases font-bold text-4xl text-white">Welcome to<br>Forage</span>
-                        <div class="font-TTInterphases text-sm text-white max-w-xs">
-                            Once you've made an account, you'll be able to write your own reviews, upvote or downvote others' reviews, register your own establishment, and get a personalized feed.
-                        </div>
-                    </div>
-                </div>
+      <div class="modal">
+        <div class="left">
+          <div class="flex-col">
+            <span class="main-text">Welcome to Forage</span>
+            <div class="sub-text">
+              Once you've made an account, you'll be able to write your own reviews, upvote or
+              downvote others' reviews, register your own establishment, and get a personalized
+              feed.
             </div>
-            <div class="mt-10 ml-20 svg">
-              <span class="font-TTInterphases font-bold text-4xl"><span class="text-[#2A7E58]">{{ type }}</span> {{ typeText }}</span>
-              <div class="mt-10">
-                <slot name="content"></slot>
-              </div>
-            </div>
+          </div>
+        </div>
+        <div class="right">
+          <span class="head">
+            <span style="color: #2a7e58">{{ type }} </span>
+            {{ typeText }}
+          </span>
+          <div class="mt-10">
+            <slot name="content"></slot>
           </div>
         </div>
       </div>
@@ -42,17 +40,13 @@ export default {
 
 <style scoped>
 .backdrop {
-    top: 0;
-    position: fixed;
-    background: rgba(0,0,0,0.5);
-    width: 100%;
-    height: 100%;
-}
-.bg {
-    background-image: url('../assets/LogRegModal/LogRegbg.png');
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: left;
-    max-height: 75vh;
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.9);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,29 +3,51 @@
     <LogRegModal type="Login" typeText="your account.">
       <template v-slot:content>
         <form @submit.prevent="submitForm">
-          <div class=" flex flex-col">
+          <div class="flex flex-col">
             <div class="flex items-start border">
-              <img src="../assets/LoginRegister/Email.png">
-              <input v-model="email" type="Email" @focus="hidePlaceholder" @blur="showPlaceholder" placeholder="Email" class=" placeholder-inherit pl-1" required>
+              <img src="../assets/LoginRegister/Email.png" />
+              <input
+                v-model="email"
+                type="Email"
+                @focus="hidePlaceholder"
+                @blur="showPlaceholder"
+                placeholder="Email"
+                class="placeholder-inherit pl-1"
+                required
+              />
             </div>
             <label v-if="emailText">{{ emailText }}</label>
-  
+
             <div class="flex items-start border">
-              <img src="../assets/LoginRegister/Key.png">
-              <input v-model="password" type="Password" @focus="hidePlaceholder" @blur="showPlaceholder" placeholder="Password" class=" placeholder-inherit pl-1"  required>
+              <img src="../assets/LoginRegister/Key.png" />
+              <input
+                v-model="password"
+                type="Password"
+                @focus="hidePlaceholder"
+                @blur="showPlaceholder"
+                placeholder="Password"
+                class="placeholder-inherit pl-1"
+                required
+              />
             </div>
             <label v-if="passwordText">{{ passwordText }}</label>
-  
+
             <div>
-              <input v-model="keepLoggedIn" type="checkbox">
-              <label class="font-TTInterphases pl-2 select-none" @click="toggleKeepLogin">Keep me logged in</label>
+              <input v-model="keepLoggedIn" type="checkbox" />
+              <label class="font-TTInterphases pl-2 select-none" @click="toggleKeepLogin"
+                >Keep me logged in</label
+              >
             </div>
 
-            <button class=" rounded-2xl bg-[#1A2228] max-w-min text-white uppercase font-SpaceGrotesk p-2 pl-5 pr-5 text-xs mb-5">Login</button>
+            <button
+              class="rounded-2xl bg-[#1A2228] max-w-min text-white uppercase font-SpaceGrotesk p-2 pl-5 pr-5 text-xs mb-5"
+            >
+              Login
+            </button>
 
-            <div class=" font-TTInterphases">
+            <div class="font-TTInterphases">
               <div>Don't have an account?</div>
-              <RouterLink to="/register" class=" text-[#2A7E58] underline">Sign up here</RouterLink>
+              <RouterLink to="/register" class="text-[#2A7E58] underline">Sign up here</RouterLink>
             </div>
           </div>
         </form>
@@ -35,8 +57,8 @@
 </template>
 
 <script>
-import LogRegModal from '../components/LogRegModal.vue';
-import { RouterLink } from 'vue-router';
+import LogRegModal from '../components/LogRegModal.vue'
+import { RouterLink } from 'vue-router'
 export default {
   components: {
     LogRegModal
@@ -58,30 +80,32 @@ export default {
   },
   methods: {
     submitForm() {
-      this.emailText = '';
-      this.passwordText = '';
-      const emailExists = this.accounts.find(account => account.email.toLowerCase() === this.email);
+      this.emailText = ''
+      this.passwordText = ''
+      const emailExists = this.accounts.find(
+        (account) => account.email.toLowerCase() === this.email
+      )
       if (!emailExists) {
-        this.emailText = 'Account does not exist!';
-      }
-      else {
-        const passwordMatch = this.accounts.find(account => account.password === this.password);
+        this.emailText = 'Account does not exist!'
+      } else {
+        const passwordMatch = this.accounts.find((account) => account.password === this.password)
         if (!passwordMatch) {
-          this.passwordText = 'Password is incorrect!';
-        }
-        else {
-          this.$router.push('/home');
+          this.passwordText = 'Password is incorrect!'
+        } else {
+          this.$router.push('/home')
         }
       }
     },
     hidePlaceholder(e) {
-      e.target.placeholder = '';
+      e.target.placeholder = ''
     },
     showPlaceholder(e) {
-      (e.target.type === 'email') ? e.target.placeholder = 'Email' : e.target.placeholder = 'Password';
+      e.target.type === 'email'
+        ? (e.target.placeholder = 'Email')
+        : (e.target.placeholder = 'Password')
     },
     toggleKeepLogin() {
-      this.keepLoggedIn = !this.keepLoggedIn;
+      this.keepLoggedIn = !this.keepLoggedIn
     }
   }
 }
@@ -89,17 +113,17 @@ export default {
 
 <style scoped>
 .backdrop {
-    top: 0;
-    position: fixed;
-    background: rgba(0,0,0,0.5);
-    width: 100%;
-    height: 100%;
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
 }
 input {
   background-color: transparent;
 }
 .border {
-  border-bottom: 1px solid #1A2228;
+  border-bottom: 1px solid #1a2228;
   margin-bottom: 1rem;
   max-width: 25vw;
 }
