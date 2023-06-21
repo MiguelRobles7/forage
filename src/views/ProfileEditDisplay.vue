b2b25c80661f79edf2d56bf40b96db29d5515ab7
Miguel Robles <donnaldmiguelrobles7@gmail.com>
2023-06-22 02:38:33 +0800

    feat: add profile edit right side

--- src/views/ProfileEditDisplay.vue
+++ src/views/ProfileEditDisplay.vue
@@ -9,7 +9,7 @@ export default {
   data() {
     return {
       profile_picture: '/src/assets/profile-edit-display/user.png',
-      banner: '/src/assets/profile-edit-display/banner.png',
+      banner: '/src/assets/profile-edit-display/cover.png',
       name: 'I miss you balik ka na',
       account_type: 'personal ',
       description:
@@ -39,7 +39,65 @@ export default {
         </div>
         <ProfileEditRedirect></ProfileEditRedirect>
       </div>
-      <div class="right">betlog</div>
+      <div class="right">
+        <div class="main-settings">
+          <h1>Display Settings</h1>
+          <div class="setting-item">
+            <span>Display Name</span>
+            <input class="profile-input" type="text" :placeholder="name" />
+          </div>
+          <div class="setting-item">
+            <span>Description</span>
+            <textarea class="profile-input" name="" id="" cols="30" rows="10" :placeholder="description"></textarea>
+          </div>
+          <div class="setting-item">
+            <span>Street Name</span>
+            <input class="profile-input" type="text" :placeholder="street" />
+          </div>
+          <div class="setting-pair">
+            <div class="setting-item">
+              <span>City</span>
+              <input class="profile-input" type="text" :placeholder="city" />
+            </div>
+            <div class="setting-item">
+              <span>Province</span>
+              <input class="profile-input" type="text" :placeholder="province" />
+            </div>
+          </div>
+          <div class="setting-pair">
+            <div class="setting-item">
+              <span>Country</span>
+              <input class="profile-input" type="text" :placeholder="country" />
+            </div>
+            <div class="setting-item">
+              <span>ZIP Code</span>
+              <input class="profile-input" type="text" :placeholder="zip_code" style="width: 28%" />
+            </div>
+          </div>
+          <div class="flex-row" style="gap: 1em; margin-top: 0.8em">
+            <button class="save-button">Save Changes</button>
+            <button class="cancel-button">Cancel</button>
+          </div>
+        </div>
+        <div class="image-settings">
+          <div class="setting-item">
+            <span>Profile Picture</span>
+            <img :src="profile_picture" alt="profile image" class="profile-image" />
+            <button class="image-button">
+              <img src="src\assets\profile-edit-redirect\general.svg" alt="" />
+              CHANGE
+            </button>
+          </div>
+          <div class="setting-item">
+            <span>Banner Picture</span>
+            <img :src="banner" alt="banner image" class="banner-image" />
+            <button class="image-button">
+              <img src="src\assets\profile-edit-redirect\general.svg" alt="" />
+              CHANGE
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
