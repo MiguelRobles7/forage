be77ddc92261faca1da6887acda8d91a1803c5d9
Miguel Robles <donnaldmiguelrobles7@gmail.com>
2023-06-23 03:03:55 +0800

    feat: add dynamic searching

--- src/router/index.js
+++ src/router/index.js
@@ -49,6 +49,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ProfileView.vue')
+    },
+    {
+      path: '/search-results',
+      name: 'search-results',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/SearchResults.vue')
     }
   ]
 })
