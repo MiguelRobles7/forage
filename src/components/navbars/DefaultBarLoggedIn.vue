376ff5d035fa38c5ee7936c9a2c04b11243d234b
Miguel Robles <donnaldmiguelrobles7@gmail.com>
2023-06-23 21:04:11 +0800

    fix: add missing designs

---
+++ src/components/navbars/DefaultBarLoggedIn.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: { name: String, image: String }
+}
+</script>
+<template>
+  <nav class="navbar" style="background-color: #0d1115">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/index"> Forage </a>
+      <div class="d-flex column-gap-3 align-items-center">
+        <button class="log-nav-button">
+          <img :src="image" alt="" />
+          <span>{{ name }}</span>
+        </button>
+        <button class="nav-button">Log Out</button>
+      </div>
+    </div>
+  </nav>
+</template>
