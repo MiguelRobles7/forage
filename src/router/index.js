0f612d87f2eb1f257eb16fc9ad0fbdb0a8eb492c
Johndayll Arizala <johndayll_arizala@dlsu.edu.ph>
2023-06-23 20:47:24 +0800

    feat: remove login and register from router

--- src/router/index.js
+++ src/router/index.js
@@ -18,22 +18,6 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LoginView.vue')
-    },
-    {
-      path: '/register',
-      name: 'register',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/RegisterView.vue')
-    },
-    {
       path: '/establishment/:id',
       name: 'establishment',
       // route level code-splitting
