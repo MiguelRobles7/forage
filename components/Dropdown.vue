ee3759c9ccdcff1af92fb5ca9c7337c672fb9fd3
Miguel Robles <donnaldmiguelrobles7@gmail.com>
2023-07-17 00:43:12 +0800

    feat: account settings page implementation

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -15,14 +15,14 @@ export default {
     </NuxtLink>
 
     <div class="dropdown-items-container">
-      <NuxtLink to="/profile/edit/1" style="display: contents">
+      <NuxtLink to="/profile/edit/display/1" style="display: contents">
         <div class="dropdown-item">
           <img class="icon" src="~/assets/icons/display.svg" alt="" />
           <span class="text"> Display Settings </span>
           <img src="~/assets/icons/d_chev_right.svg" alt="" />
         </div>
       </NuxtLink>
-      <NuxtLink to="/profile/edit/1" style="display: contents">
+      <NuxtLink to="/profile/edit/account/1" style="display: contents">
         <div class="dropdown-item">
           <img class="icon" src="~/assets/icons/account-setting.svg" alt="" />
           <span class="text">Account Settings </span>
