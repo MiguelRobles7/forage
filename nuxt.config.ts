a1c8c60d280ea77380ab11f0a2c262137c199b60
Johndayll Arizala <johndayll_arizala@dlsu.edu.ph>
2023-07-18 00:11:37 +0800

    refactor: remove all traces of mongodb

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,5 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/node_modules/bootstrap/dist/css/bootstrap.min.css', '~/assets/styles/style.scss'],
-
-  runtimeConfig: {
-    mongoUrl:
-      'mongodb+srv://forage-dev:p0g1-n10ng-lht-0nG-lg8-22oh-HD@forage.khfekqf.mongodb.net/forage?retryWrites=true&w=majority'
-  },
-
   modules: ['@nuxtjs/supabase']
 })
