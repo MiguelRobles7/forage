1edecb0647ed861c97ed42831cd77192502e30e0
Rafa <58285574+JsRphlMrtnz@users.noreply.github.com>
2023-07-20 16:55:08 +0800

    fix: user endpoint id

--- server/api/users/[id].get.js
+++ server/api/users/[id].get.js
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event) => {
     console.log("Getting a certain user..");
     const model = serverSupabaseClient(event)
     const id = event.context.params.id;
-    const { data, error } = await model.from('profiles').select().eq('id', id);
+    const { data, error } = await model.from('profiles').select().eq('profile_id', id);
     if(error) {
         console.log(error);
         return "";
