b8c67db8b8266e9e61ee782056dcbd14a6fbb864
MiguelRobles7 <donnaldmiguelrobles7@gmail.com>
2023-07-24 02:21:33 +0800

    fix: dropdown hard links

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -3,30 +3,30 @@ export default {
   data() {
     return {
       user: {
-        name: String, 
+        name: String,
         dpLink: String,
-      },
+        id: Number
+      }
     }
   },
-  async setup()  {
-
-  },
+  async setup() {},
   async created() {
-    const supabase = useSupabaseClient();
-    var supabaseSession = await supabase.auth.getSession();
-    var userSession = null;
-    var userId = "";
+    const supabase = useSupabaseClient()
+    var supabaseSession = await supabase.auth.getSession()
+    var userSession = null
+    var userId = ''
 
     if (!supabaseSession.data.session) {
       this.isLoggedIn = false
     } else {
       this.isLoggedIn = true
       userSession = supabaseSession.data.session.user
-      userId = userSession.id;
-      const userRequest = await useFetch(`/api/users/session/${userId}`);
-      const userData = userRequest.data.value.users[0];
-      this.user.name = userData.name;
-      this.user.dpLink = userData.displayPicture;
+      userId = userSession.id
+      const userRequest = await useFetch(`/api/users/session/${userId}`)
+      const userData = userRequest.data.value.users[0]
+      this.user.name = userData.name
+      this.user.dpLink = userData.displayPicture
+      this.id = userData.profile_id
     }
   },
   methods: {
@@ -41,7 +41,7 @@ export default {
 </script>
 <template>
   <div class="dropdown">
-    <NuxtLink to="/profile/view/1" style="display: contents">
+    <NuxtLink :to="`/profile/view/${id}`" style="display: contents">
       <div class="dropdown-head">
         <img class="head-pfp" :src="user.dpLink" alt="" />
         <span class="head-span">{{ user.name }} </span>
@@ -49,7 +49,7 @@ export default {
     </NuxtLink>
 
     <div class="dropdown-items-container">
-      <NuxtLink to="/profile/edit/display/1" style="display: contents">
+      <NuxtLink :to="`/profile/edit/display/${id}`" style="display: contents">
         <div class="dropdown-item">
           <img class="icon" src="~/assets/icons/display.svg" alt="" />
           <span class="text"> Display Settings </span>
@@ -64,7 +64,7 @@ export default {
         </div>
       </NuxtLink> -->
 
-      <NuxtLink to="/profile/manage-reviews/1" style="display: contents">
+      <NuxtLink :to="`/profile/manage-reviews/${id}`" style="display: contents">
         <div class="dropdown-item">
           <img class="icon" src="~/assets/icons/manage-reviews.svg" alt="" />
           <span class="text"> Manage Reviews </span>
