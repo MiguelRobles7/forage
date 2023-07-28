acf0b44fbb0737c2c4e76e9c1a6154ba56c65b72
MiguelRobles7 <donnaldmiguelrobles7@gmail.com>
2023-07-29 00:17:46 +0800

    feat: proper loading page

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,25 +1,42 @@
 <template>
-    <div class="backdrop">
-        <div class="loading">
-            Loading lang idol...
-        </div>
+  <div class="backdrop">
+    <div class="center-load" style="display: flex; flex-direction: column">
+      <img class="loading-gif" src="/assets/images/loading.gif" alt="" />
+      <div class="loading title">Loading Page</div>
+      <div>Please allow up to 20 seconds while we're getting things ready for you</div>
     </div>
+  </div>
 </template>
 
 <style scoped>
 .backdrop {
-    position: fixed;
-    top: 0;
-    background: black;
-    width: 100vw;
-    height: 100vh;
-    z-index: 50;
+  position: fixed;
+  top: 0;
+  background: black;
+  width: 100vw;
+  height: 100vh;
+  z-index: 50;
+  align-items: center;
+  justify-content: center;
+}
+.loading-gif {
+  max-width: 15rem;
+  max-height: 15rem;
+  justify-content: center;
+  align-self: center;
 }
 .loading {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center; 
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  font-family: TTInterphases;
+  margin: 0;
+}
+.center-load {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
